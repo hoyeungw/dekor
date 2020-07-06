3f440a43b0cb6b4661f310ae943d5e10807d69c8
hazen <hoyeungw@outlook.com>
2020-07-06 17:00:16 +0800

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
+import babel               from '@rollup/plugin-babel'
 import commonjs            from '@rollup/plugin-commonjs'
 import json                from '@rollup/plugin-json'
 import nodeResolve         from '@rollup/plugin-node-resolve'
 import { decoObject, ros } from '@spare/logger'
-import babel               from 'rollup-plugin-babel'
 import fileInfo            from 'rollup-plugin-fileinfo'
 
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
@@ -14,6 +14,7 @@ const babelPluginOptions = {
   babelrc: false,
   comments: true,
   sourceMap: true,
+  babelHelpers: 'bundled',
   exclude: 'node_modules/**',
   plugins: [
     // ['@babel/plugin-transform-runtime', { helpers: false, }],
